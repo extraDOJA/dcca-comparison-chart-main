354db4276b12dd9e133f47d9c25abf352d543e81
extraDOJA <84607802+extraDOJA@users.noreply.github.com>
2025-12-17 06:28:40 -0400

    Uploaded previous files

---
+++ login.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+
+# login.py
+
+def load_authenticator():
+    """Load and return an authenticator object from config.yaml."""
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
+    return authenticator
